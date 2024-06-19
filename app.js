a7494a127533b3f24000f6e66cc65af72ce96195
jlakvidu <jlakvidu@gmail.com>
2024-06-19 11:08:22 +0530

    add animation

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 function calc() {
-    let num1 = new Number(document.getElementById("num1").value);
-    let num2 = new Number(document.getElementById("num2").value);
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
     let operator = document.getElementById("operator").value;
 
     let lblOutput = document.getElementById("output");
 
-    switch(operator) {
-        case "+" : lblOutput.innerHTML = num1 + num2;
-        break;
-        case "-" : lblOutput.innerHTML = num1 - num2;
-        break;
-        case "*" : lblOutput.innerHTML = num1 * num2;
-        break;
-        case "/" : lblOutput.innerHTML = num1 / num2;
-        break;
-        default : lblOutput.innerHTML = "Invalid Operator...";
+    switch (operator) {
+        case "+":
+            lblOutput.innerHTML = num1 + num2;
+            break;
+        case "-":
+            lblOutput.innerHTML = num1 - num2;
+            break;
+        case "*":
+            lblOutput.innerHTML = num1 * num2;
+            break;
+        case "/":
+            lblOutput.innerHTML = num1 / num2;
+            break;
+        default:
+            lblOutput.innerHTML = "Invalid Operator...";
     }
+
+    lblOutput.classList.add('animate-output');
+    setTimeout(() => {
+        lblOutput.classList.remove('animate-output');
+    }, 500);
 }
